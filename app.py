846ce58c0976bd8d04645c63ef0bf69f60aca892
Ashwath <ashwathguru.s@gmail.com>
2023-12-01 23:22:04 +0530

    transcript move 3

--- app.py
+++ app.py
@@ -126,14 +126,14 @@ def reportsGPT():
             #st.audio(audio_bytes, format="audio/wav")
             # Display the recorded video
             # HTML template with autoplay and loop attributes
-            video_path = "images/character_video.mp4"
-            video_html = f"""
-            video width="100%" controls autoplay loop>
-             <source src="{video_path}" type="video/mp4">
-             Your browser does not support the video tag.
-            /video>
-            """.format(video_path=video_path)
-            components.html(video_html, width=100, height=100)
+            #video_path = "images/character_video.mp4"
+            #video_html = f"""
+            #video width="100%" controls autoplay loop>
+            # <source src="{video_path}" type="video/mp4">
+            # Your browser does not support the video tag.
+            #/video>
+            #""".format(video_path=video_path)
+            #components.html(video_html, width=100, height=100)
             
             #st.video(video_path, start_time=0)
             save_audio_file(audio_bytes, "mp3")
