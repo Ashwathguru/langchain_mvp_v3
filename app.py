c264ec03ec4530e863b1bae8ddcef08b6f2c7e0d
Ashwath <ashwathguru.s@gmail.com>
2023-12-02 10:39:14 +0530

    re size gif 3

--- app.py
+++ app.py
@@ -117,10 +117,9 @@ def reportsGPT():
             col1,col3 = st.columns([1, 1])
             with col1:
                 #st.markdown("![Alt Text](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcHU0am5mY2ZnczV5aHFnNnB0bGM2aWNkYmx4c2JneTZqeWljNXY4eiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/eFGDIwEIxtWrYDaOA3/giphy.gif)")
-                st.markdown("![Alt Text](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbHY1OWI5NDU2ZXBudGZzc2twbHRlM2cyM2g2ZHE4eDJnN3Z1Y2FjZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/sUMxsGhGNlCTIq8wyv/giphy.gif)")
-                
                 subcol1_1, subcol2_1, subcol3_1 = st.columns([1, 1, 1])
                 with subcol2_1:
+                    st.markdown("![Alt Text](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbHY1OWI5NDU2ZXBudGZzc2twbHRlM2cyM2g2ZHE4eDJnN3Z1Y2FjZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/sUMxsGhGNlCTIq8wyv/giphy.gif)")
                     audio_bytes = audio_recorder(text="")
                     if audio_bytes:
                         st.cache_data.clear()
