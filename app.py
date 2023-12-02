6197a61bc528dd1d83fa10563179ba23b058d8cb
Ashwath <ashwathguru.s@gmail.com>
2023-12-02 10:44:29 +0530

    revert

--- app.py
+++ app.py
@@ -117,39 +117,38 @@ def reportsGPT():
             col1,col3 = st.columns([1, 1])
             with col1:
                 #st.markdown("![Alt Text](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcHU0am5mY2ZnczV5aHFnNnB0bGM2aWNkYmx4c2JneTZqeWljNXY4eiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/eFGDIwEIxtWrYDaOA3/giphy.gif)")
+                st.markdown("![Alt Text](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbHY1OWI5NDU2ZXBudGZzc2twbHRlM2cyM2g2ZHE4eDJnN3Z1Y2FjZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/sUMxsGhGNlCTIq8wyv/giphy.gif)")
                 subcol1_1, subcol2_1, subcol3_1 = st.columns([1, 1, 1])
                 with subcol2_1:
-                    st.markdown("![Alt Text](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbHY1OWI5NDU2ZXBudGZzc2twbHRlM2cyM2g2ZHE4eDJnN3Z1Y2FjZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/sUMxsGhGNlCTIq8wyv/giphy.gif)")
-                    subcol2_1_1, subcol2_1_2, subcol3_1_3 = st.columns([1, 1, 1])
-                    with subcol2_1_2:
-                        audio_bytes = audio_recorder(text="")
-                        if audio_bytes:
-                            st.cache_data.clear()
-                            save_audio_file(audio_bytes, "mp3")
-                            audio_file_path = max(
-                                [f for f in os.listdir(".") if f.startswith("audio")],
-                                key=os.path.getctime,
-                            )
-                            # Transcribe the audio file
-                            transcript_text = transcribe_audio(audio_file_path)
-                            if transcript_text != "I'm sorry, I couldn't catch that. Could you please repeat your question?":
-                                with col3:
-                                    # Display the transcript
-                                    st.header("Transcript",divider="red")
-                                    st.header(transcript_text)
-                                    query=transcript_text
-                                    response=get_answer_csv(query)
-                                    if response != "":
-                                        resp = ":green["+response+"]"
-                                        st.header(resp)
-                                        js_code="""
-                                        var u = new SpeechSynthesisUtterance();
-                                        u.text = "{response}";
-                                        u.lang = 'en-US';
-                                        speechSynthesis.speak(u);
-                                        """.format(response=response)
-                                        my_html = f"<script>{js_code}</script>"
-                                        components.html(my_html, width=0, height=0)
+                    
+                    audio_bytes = audio_recorder(text="")
+                    if audio_bytes:
+                        st.cache_data.clear()
+                        save_audio_file(audio_bytes, "mp3")
+                        audio_file_path = max(
+                            [f for f in os.listdir(".") if f.startswith("audio")],
+                            key=os.path.getctime,
+                        )
+                        # Transcribe the audio file
+                        transcript_text = transcribe_audio(audio_file_path)
+                        if transcript_text != "I'm sorry, I couldn't catch that. Could you please repeat your question?":
+                            with col3:
+                                # Display the transcript
+                                st.header("Transcript",divider="red")
+                                st.header(transcript_text)
+                                query=transcript_text
+                                response=get_answer_csv(query)
+                                if response != "":
+                                    resp = ":green["+response+"]"
+                                    st.header(resp)
+                                    js_code="""
+                                    var u = new SpeechSynthesisUtterance();
+                                    u.text = "{response}";
+                                    u.lang = 'en-US';
+                                    speechSynthesis.speak(u);
+                                    """.format(response=response)
+                                    my_html = f"<script>{js_code}</script>"
+                                    components.html(my_html, width=0, height=0)
             #st.image("images/report_charts.jpg", use_column_width="always")
         #Chat Tab
         with tab2:
@@ -168,12 +167,12 @@ def reportsGPT():
 working_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(working_dir)
 # Run the main function
-#try:
-#    reportsGPT()
-#except Exception as e:
-#    # Handle other exceptions
-#    #logging.info(e)
-#    print(f"An error occurred(Please refresh and try): {e}")
-#    st.info("We ran into a problem. We're still in beta. Please refresh and try!")
+try:
+    reportsGPT()
+except Exception as e:
+    # Handle other exceptions
+    #logging.info(e)
+    print(f"An error occurred(Please refresh and try): {e}")
+    st.info("We ran into a problem. We're still in beta. Please refresh and try!")
 
-reportsGPT()
+#reportsGPT()
